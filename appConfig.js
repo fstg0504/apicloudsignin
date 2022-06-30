4b091e769a333bf18f904cc2950a60fde760a278
fstgshow <16302565+fstg0504@users.noreply.github.com>
2022-06-30 16:44:23 +0800

    feat:增加cookie

--- appConfig.js
+++ appConfig.js
@@ -1,6 +1,6 @@
 const userConfig = {
     sid: '',
-    cookie: '',
+    cookie: 'ul_token=s%3ApHcmnLLwZ2iOyJr6IEEt.5pIC%2FjcYGW%2FvDkeerMUIAroI1TN2B7GhDdyiqsgmwuQ; connect.sid=s%3AlggbvCZN1rwbw3YjfkkLidGd0i5FDSlg.z9qGrqEgLeXsMt692tzUej7EzHdnKHjgb3ksPY8dB6k;',
 }
 const API = {
     checkin: 'https://www.apicloud.com/api/user/checkin',
